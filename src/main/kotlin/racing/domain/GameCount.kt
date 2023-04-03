8b1f7466a8963cae562d90907060848e55f3a380
inseonyun <yuninseon@naver.com>
2023-04-04 02:26:36 +0900

    refactor(GameCount, InputView): 게임 시도 횟수 검증 로직 제거

--- src/main/kotlin/racing/domain/GameCount.kt
+++ src/main/kotlin/racing/domain/GameCount.kt
@@ -2,13 +2,5 @@ package racing.domain
 
 @JvmInline
 value class GameCount(val value: Int) {
-    init {
-        require(value >= MIN_GAME_COUNT) { ERROR_GAME_COUNT }
-    }
     operator fun dec(): GameCount = GameCount(value - 1)
-
-    companion object {
-        private const val MIN_GAME_COUNT = 1
-        private const val ERROR_GAME_COUNT = "최소 게임 시도 횟수는 1회입니다"
-    }
 }
--- src/main/kotlin/racing/view/InputView.kt
+++ src/main/kotlin/racing/view/InputView.kt
@@ -17,7 +17,9 @@ class InputView {
     fun readGameCount(): GameCount =
         runCatching {
             println(MESSAGE_INPUT_GAME_COUNT)
-            GameCount(readln().toInt())
+            val gameCount = readln().toInt()
+            require(gameCount >= MIN_GAME_COUNT) { ERROR_GAME_COUNT }
+            GameCount(gameCount)
         }
             .onFailure {
                 println(it.message)
@@ -25,7 +27,9 @@ class InputView {
             .getOrDefault(readGameCount())
 
     companion object {
+        private const val MIN_GAME_COUNT = 1
         private const val MESSAGE_INPUT_NAMES = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분)."
         private const val MESSAGE_INPUT_GAME_COUNT = "시도할 횟수는 몇 회인가요?"
+        private const val ERROR_GAME_COUNT = "최소 게임 시도 횟수는 1회입니다"
     }
 }
