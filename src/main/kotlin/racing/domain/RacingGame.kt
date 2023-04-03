1b7088800f0c5f0073683de9a21e36277e1abf63
inseonyun <yuninseon@naver.com>
2023-04-04 02:43:39 +0900

    refactor(RacingGame): 최종 우승자 계산 로직 구현

--- src/main/kotlin/racing/domain/RacingGame.kt
+++ src/main/kotlin/racing/domain/RacingGame.kt
@@ -4,8 +4,13 @@ class RacingGame(val cars: Cars, var gameCount: GameCount, val numberGenerator: 
     fun run(statusOfCars: (Cars) -> Unit) {
         while (gameCount--.value > 0) {
             startMatchOnce()
+            statusOfCars(cars)
         }
-        statusOfCars(cars)
+    }
+
+    fun determineWinners(): Names {
+        val max = cars.value.maxBy { it.position.value }.position
+        return Names(cars.value.filter { it.position == max }.map { it.name })
     }
 
     private fun startMatchOnce() {
