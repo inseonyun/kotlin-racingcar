6aa299325a47a3f1c82bb528c66fa48dc1080e97
inseonyun <yuninseon@naver.com>
2023-04-04 21:56:45 +0900

    refactor(NameTest): 테스트명 수정

--- src/test/kotlin/racing/domain/NameTest.kt
+++ src/test/kotlin/racing/domain/NameTest.kt
@@ -7,7 +7,7 @@ import org.junit.jupiter.params.provider.ValueSource
 class NameTest {
     @ParameterizedTest
     @ValueSource(strings = ["", "banana"])
-    fun `이름이 6자면 예외가 발생한다`(name: String) {
+    fun `이름이 1자 이상 5자 이하가 아니면 예외가 발생한다`(name: String) {
         assertThrows<IllegalArgumentException> { Name(name) }
     }
 }
